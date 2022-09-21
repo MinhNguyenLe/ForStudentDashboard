cb598df78fd49afcb28ab2bcff87f93f99aff4b9
Tien_Nguyen <18521487@gm.uit.edu.vn>
2022-09-21 22:38:34 +0700

    upgraded: version type react

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,11 +1,11 @@
-import { FC, useState, createContext, useEffect } from 'react';
+import { useState, createContext, useEffect, ReactNode } from 'react';
 import { ThemeProvider } from '@mui/material';
 import { themeCreator } from './base';
 import { StylesProvider } from '@mui/styles';
 
 export const ThemeContext = createContext((_themeName: string): void => {});
 
-const ThemeProviderWrapper: FC = (props) => {
+const ThemeProviderWrapper = ({ children }: { children: ReactNode }) => {
   const [themeName, _setThemeName] = useState('PureLightTheme');
 
   useEffect(() => {
@@ -23,7 +23,7 @@ const ThemeProviderWrapper: FC = (props) => {
   return (
     <StylesProvider injectFirst>
       <ThemeContext.Provider value={setThemeName}>
-        <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
       </ThemeContext.Provider>
     </StylesProvider>
   );
